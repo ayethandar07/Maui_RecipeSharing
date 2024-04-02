535178bf54464a308dde3851b29f651818b2ce67
ayethandar07 <athandar1998@gmail.com>
2024-04-02 14:58:51 +0630

    Add project files.

---
+++ Maui_RecipeDemo/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_RecipeDemo.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public string Amount { get; set; }
+    }
+}
---
+++ Maui_RecipeDemo/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_RecipeDemo.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<string> Method { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
